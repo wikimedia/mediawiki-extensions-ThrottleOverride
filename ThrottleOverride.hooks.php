7c83c184638ec865eaac044832cf0810005ba1d6
Bryan Davis <bd808@wikimedia.org>
2017-11-20 18:43:45 -0700

    Use WANCache for throttle lookups
    Check for throttle records in the configured WANCache instance and cache
    new database lookup results on cache miss. Cached data is not purged
    when the backing database record is edited, so the cache duration is low
    (1 hour).
    
    Bug: T147362
    Change-Id: I11aa1eb5c13edb3638970fcba4d41a689bc106f7

--- ThrottleOverride.hooks.php
+++ ThrottleOverride.hooks.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * MediaWiki extension to temporarily lift account creation throttles.
  * Copyright (C) 2013 Tyler Romeo <tylerromeo@gmail.com>
@@ -21,6 +20,9 @@
 use MediaWiki\Logger\LoggerFactory;
 
 class ThrottleOverrideHooks {
+
+	const NO_OVERRIDE = -1;
+
 	/**
 	 * @param string $ip
 	 * @return bool
@@ -48,32 +50,56 @@ class ThrottleOverrideHooks {
 			return true;
 		}
 
-		$dbr = wfGetDB( DB_REPLICA );
-
 		if ( $user->isAnon() && IP::isValid( $user->getName() ) ) {
 			$ip = $user->getName();
 		} elseif ( $ip === null ) {
 			$ip = RequestContext::getMain()->getRequest()->getIP();
 		}
+		$hexIp = IP::toHex( $ip );
 
-		$quotedIp = $dbr->addQuotes( IP::toHex( $ip ) );
-		$conds = [
-			"thr_range_start <= $quotedIp",
-			"thr_range_end >= $quotedIp",
-			'thr_type' . $dbr->buildLike( $dbr->anyString(), $action, $dbr->anyString() )
-		];
+		$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+		$expiry = $cache->getWithSetCallback(
+			$cache->makeKey( 'throttle_override', $action, $hexIp ),
+			$cache::TTL_HOUR,
+			function ( $cValue, &$ttl, &$setOpts, $asOf ) use ( $hexIp, $action ) {
+				$dbr = wfGetDB( DB_REPLICA );
+				$setOpts += Database::getCacheSetOptions( $dbr );
 
-		$expiry = $dbr->selectField(
-			'throttle_override',
-			'thr_expiry',
-			$conds,
-			__METHOD__,
-			[ 'ORDER BY' => 'thr_expiry DESC' ]
+				$expiry = $dbr->selectField(
+					'throttle_override',
+					'thr_expiry',
+					ThrottleOverrideHooks::makeConds( $dbr, $hexIp, $action ),
+					'ThrottleOverrideHooks::onPingLimiter',
+					[ 'ORDER BY' => 'thr_expiry DESC' ]
+				);
+
+				// Its tempting to set the TTL to match the expiration we
+				// found in the DB, but since the record is editable and we do
+				// not purge every key in the range when it changes we will
+				// just leave the default cache time alone. The exception to
+				// this rule is when we are caching a row which will expire in
+				// less than the default TTL.
+				// NOTE: this means that changes to an existing record may not
+				// effect all IPs in the range equally until the default cache
+				// period has elapsed.
+				if ( $expiry !== false ) {
+					// An override exists; do not cache for more than the
+					// override's current-time-left
+					$nowUnix = time();
+					$overrideCTL = wfTimestamp( TS_UNIX, $expiry ) - $nowUnix;
+					$ttl = min( $ttl, max( $overrideCTL, 1 ) );
+				}
+
+				// If we return false the value will not be cached
+				return ( $expiry === false ) ? self::NO_OVERRIDE : $expiry;
+			}
 		);
 
-		if ( $expiry > wfTimestampNow() ) {
+		if ( $expiry === self::NO_OVERRIDE ) {
+			// We checked the database and found no record
+			return true;
+		} elseif ( $expiry > wfTimestampNow() ) {
 			// Valid exemption. Disable the throttle.
-
 			$logger = LoggerFactory::getInstance( 'throttleOverride' );
 			$logger->info( 'User {user} (ip: {ip}) exempted from throttle {action}', [
 				'user' => $user,
@@ -85,14 +111,33 @@ class ThrottleOverrideHooks {
 			return false;
 		} elseif ( $expiry !== false ) {
 			// Expired exemption. Delete it from the DB.
-			wfGetDB( DB_MASTER )->delete(
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->delete(
 				'throttle_override',
-				$conds,
+				self::makeConds( $dbw, $hexIp, $action ),
 				__METHOD__
 			);
 		}
 
 		return true;
+	}
+
+	/**
+	 * Make SQL query conditions.
+	 *
+	 * @param \Wikimedia\Rdbms\Database $db Database
+	 * @param string $hexIp IP address in hex string format
+	 * @param string $action Throttle action
+	 * @return array Conditions
+	 */
+	private static function makeConds( $db, $hexIp, $action ) {
+		$quotedIp = $db->addQuotes( $hexIp );
+		return [
+			"thr_range_start <= $quotedIp",
+			"thr_range_end >= $quotedIp",
+			'thr_type' . $db->buildLike(
+				$db->anyString(), $action, $db->anyString() ),
+		];
 	}
 
 	/**
