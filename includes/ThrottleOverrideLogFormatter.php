89c1f9c38f0ffb9ffcbb6360b20e6c15ab577acf
Kunal Mehta <legoktm@member.fsf.org>
2018-05-24 21:45:48 -0700

    Fix MediaWiki.Commenting.LicenseComment.InvalidLicenseTag issue
    Change-Id: Id098919c5beac1d9371f2ee948610220ff724ae3

--- includes/ThrottleOverrideLogFormatter.php
+++ includes/ThrottleOverrideLogFormatter.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0 or later
+ * @license GPL-3.0-or-later
  */
 
 /**
